1a87056b9c3e26dcbaf6b07f029ae476e0c63186
Edwin Fajardo <contacto@edwinfajardo.com>
2023-04-03 11:03:09 -0600

    Exclusión de historial de modificaciones en la base de datos; revisión de estilos en switches

--- db/mysql/historical/2022/202202_updates.sql
+++
@@ -1,6 +0,0 @@
--- Actualizaciones de base de datos en el mes de febrero de 2022
--- Por: Edwin Fajardo
--- 2022-02-07
-ALTER TABLE `user_sessions` CHANGE `branch_id` `branch_id` INT(11) NULL COMMENT 'Sucursal en la que inició sesión';
--- Nahutech Local Test
--- Teleinf Local test

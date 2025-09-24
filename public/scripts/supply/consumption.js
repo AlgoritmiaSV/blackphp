0a50fd1238eb8d207d5a32dceaa86ec80157c5ad
Edwin Fajardo <contacto@edwinfajardo.com>
2025-09-24 08:29:23 -0600

    Actualización de scripts de cálculos de formularios

--- public/scripts/supply/consumption.js
+++ public/scripts/supply/consumption.js
@@ -1,4 +1,5 @@
-start_calc_consumption = function() {
+start_calc_consumption = function()
+{
 	$(".current").on("change", function() {
 		tr = $(this).closest("tr");
 		var previous_value = parseInt(tr.find(".previous").text());
@@ -27,3 +28,38 @@ start_calc_consumption = function() {
 		}
 	});
 };
+
+$(function() {
+	$(".current_reading").on("change", function()
+	{
+		const previous_value = parseInt($(".previous_reading").val());
+		const current_value = parseInt($(this).val());
+		let consumption_input = $(".consumption");
+		if(isNaN(previous_value) || isNaN(current_value))
+		{
+			return false;
+		}
+		let consumption = current_value - previous_value;
+		if(consumption < 0)
+		{
+			$.jAlert({
+				'title': "Error",
+				'content': "La lectura no puede ser inferior a " + previous_value + ".",
+				'theme': "red",
+				'autofocus': '.jalert_accept',
+				'btns': [
+					{'text':'Aceptar', 'closeAlert':true, 'theme': 'red', 'class': 'jalert_accept'}]
+			});
+			consumption_input.val("");
+		}
+		else
+		{
+			consumption_input.val(consumption);
+		}
+	});
+
+	$(".previous_reading").on("change", function()
+	{
+		$(".current_reading").attr("min", $(this).val());
+	});
+});
